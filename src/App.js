af429fd22846a4a59aa60551eedfa3ba0f88c127
jsm <gkswk89@naver.com>
2020-02-06 11:16:25 +0900

    Apply material

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
 
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto'
+  },
+  table: {
+    minWidth: 1080 // 최소크기 유지
+  }
+})
 const customers = [{
   'id' : 1,
   'image' : 'https://placeimg.com/64/64/1',
@@ -29,25 +45,39 @@ const customers = [{
 }
 ]
 
+
 class App extends Component {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        {
-          customers.map(c => {
+      <Paper className={classes.root}>
+          <Table className={classes.table}>
+            <TableHead>
+              <TableRow>
+                <TableCell>번호</TableCell>
+                <TableCell>이미지</TableCell>
+                <TableCell>이름</TableCell>
+                <TableCell>생년월일</TableCell>
+                <TableCell>성별</TableCell>
+                <TableCell>직업</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+            {customers.map(C => {
             return (
               <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
+                key={C.id}
+                id={C.id}
+                image={C.image}
+                name={C.name}
+                birthday={C.birthday}
+                gender={C.gender}
+                job={C.job}
               />
-            )
-          })
-        }
+            )}
+          )}
+            </TableBody>
+          </Table>
       {/* <Customer 
         id={customers[0].id}
         image={customers[0].image}
@@ -72,9 +102,9 @@ class App extends Component {
         gender={customers[2].gender}
         job={customers[2].job}
       /> */}
-      </div>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
