96ad426d26365de73dce6e7136543636611d9891
jsm <gkswk89@naver.com>
2020-02-10 21:06:24 +0900

    add delete

--- client/src/App.js
+++ client/src/App.js
@@ -82,12 +82,14 @@ class App extends Component {
                 <TableCell>생년월일</TableCell>
                 <TableCell>성별</TableCell>
                 <TableCell>직업</TableCell>
+                <TableCell>설정</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>
             {this.state.customers ? this.state.customers.map(C => {
             return (
               <Customer
+                stateRefresh={this.stateRefresh}
                 key={C.id}
                 id={C.id}
                 image={C.image}
--- client/src/components/Customer.js
+++ client/src/components/Customer.js
@@ -1,6 +1,7 @@
  import React from 'react';
  import TableRow from '@material-ui/core/TableRow';
  import TableCell from '@material-ui/core/TableCell';
+ import CustomerDelete from './CustomerDelete';
 
 class  Customer extends React.Component{ // React의 라이브러리, 클래스
     render() {
@@ -12,8 +13,8 @@ class  Customer extends React.Component{ // React의 라이브러리, 클래스
                 <TableCell>{this.props.birthday}</TableCell>
                 <TableCell>{this.props.gender}</TableCell>
                 <TableCell>{this.props.job}</TableCell>
+                <TableCell><CustomerDelete stateRefresh={this.props.stateRefresh} id={this.props.id}/></TableCell>
             </TableRow>
-            
         );
     }
 }
